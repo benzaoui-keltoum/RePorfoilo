5c4b2f6417f8b49f70e754ce5c62c1d2361feebe
core.pager=cat <benzaouikeltoum724@gmail.com>
2022-11-29 20:15:24 +0100

    init

---
+++ src/resumeData.js
@@ -0,0 +1,134 @@
+let resumeData = {
+  imagebaseurl: "./../public/images/portfolio/girl.jpg",
+  name: "BENZAOUI Keltoum",
+  role: "Frontend Developer ",
+  phone: "0 798 16 27 03",
+  linkedinId: "keltoum-benzaoui",
+  skypeid: "Your skypeid",
+  roleDescription:
+    "Web Developer specialized in front end development. Motivated to learn more about web technologies.",
+
+  socialLinks: [
+    {
+      name: "linkedin",
+      url: "https://www.linkedin.com/in/keltoum-benzaoui-30276a246/",
+      className: "fa fa-linkedin",
+    },
+    {
+      name: "github",
+      url: "https://github.com/benzaoui-keltoum",
+      className: "fa fa-github",
+    },
+  ],
+  aboutme:
+    " I ' m a  software Engineer . My interest in web development sparked when I was at university.  I like building web Application  and Mobile Application ",
+  address: "Said-Hamdine Algiers, Algeria",
+  website: "https://github.com/benzaoui-keltoum",
+  education: [
+    {
+      UniversityName: "Go-My-Code School",
+      specialization: "Full Stack Java Script Developer",
+      MonthOfPassing: "Nov",
+      YearOfPassing: "2022",
+      Achievements: "Diplomat",
+    },
+    {
+      UniversityName:
+        "Université des Sciences et de la Technologie Houari-Boumediene             ",
+      specialization: "Software Developpement",
+      MonthOfPassing: "Jul",
+      YearOfPassing: "2015",
+      Achievements: "Master",
+    },
+    {
+      UniversityName:
+        "Université des Sciences et de la Technologie Houari-Boumediene  USTHB",
+      specialization: "Information System and Developpemnt",
+      MonthOfPassing: "Jul",
+      YearOfPassing: "2013",
+      Achievements: "Licence",
+    },
+  ],
+  work: [
+    {
+      CompanyName: "Private School",
+      specialization: "IT Teacher",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2022",
+      Achievements: "Web Courses",
+    },
+    {
+      CompanyName: "MDF",
+      specialization: "IT engineer",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2019",
+      Achievements: "Web Application",
+    },
+  ],
+  skillsDescription: "",
+  skills: [
+    {
+      skillname: "Bootstrap",
+    },
+
+    {
+      skillname: "JavaScript",
+    },
+
+    {
+      skillname: "Reactjs",
+    },
+
+    {
+      skillname: "Python",
+    },
+
+    {
+      skillname: "Java",
+    },
+
+    {
+      skillname: "MySQL",
+    },
+
+    {
+      skillname: "SQL",
+    },
+
+    {
+      skillname: "OOP",
+    },
+
+    {
+      skillname: "Github",
+    },
+
+    {
+      skillname: "Agile",
+    },
+  ],
+  portfolio: [
+    {
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/me2.jpg",
+    },
+    {
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+};
+
+export default resumeData;
